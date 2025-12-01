7139b52a26fdc3a9c1efc1486cf7afec71aa449c
Victor Umali <victormalunesumali@gmail.com>
2025-12-01 14:19:26 +0800

    Initial project scaffold

---
+++ blog/index.php
@@ -0,0 +1,10 @@
+ <?php include '../includes/header.php'; ?>
+<html>
+<head><title>Microblog</title><link rel="stylesheet" href="../css/styles.css"></head>
+<body>
+<h1>Microblogging Page</h1>
+<!-- Blog posts will go here -->
+<?php include '../includes/footer.php'; ?>
+<script src="../js/scripts.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+ <?php include 'includes/header.php'; ?>
+<html>
+<head><title>Fusion I.T. Solution</title><link rel="stylesheet" href="css/styles.css"></head>
+<body>
+<section id="hero"> <!-- Parallax hero section placeholder -->
+<h1>Welcome to Fusion I.T. Solution</h1>
+</section>
+<?php include 'includes/footer.php'; ?>
+<script src="js/scripts.js"></script>
+</body>
+</html>
